30aaf70145d05fcd0692d395e8b765d9b5c8df43
ndyanx <randybryan21ic@gmail.com>
2025-07-01 11:56:06 -0500

    202507011156

--- src/middlewares/validateWordParam.js
+++ src/middlewares/validateWordParam.js
@@ -1,7 +1,7 @@
 function validateWordParam(req, res, next) {
   const { word } = req.params;
   console.log(word);
-  if (!word || typeof word !== 'string' || !/^[a-zA-Z]+$/.test(word)) {
+  if (!word || typeof word !== 'string' || !/^[a-zA-Z']+$/.test(word)) {
     return res.status(400).json({ error: 'Invalid word. Only letters are allowed.' });
   }
 
